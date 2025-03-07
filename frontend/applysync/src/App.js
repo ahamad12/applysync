17ae10e293b52ef3db7b6f151c60bbe8a02ac773
ahamad12 <it20225292@my.sliit.lk>
2025-03-08 02:19:16 +0530

    Initial commit

---
+++ frontend/applysync/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import JobApplicationForm from './components/JobApplicationForm';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <JobApplicationForm />
+    </div>
+  );
+}
+
+export default App;
